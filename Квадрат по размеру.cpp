9e4f122f74c200a2f98132f3cf8ce1c49bf4ba58
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2020-10-14 12:10:51 +0300

    Add files via upload

---
+++ Квадрат по размеру.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "Square by size." << endl;
+	int i; cout << "Enter size: "; cin >> i;
+	for (int x = 0;x < i;x++)
+	{
+		for (int y = 0;y < i;y++)
+		{
+			if ((x == 0) || (x == (i - 1)) || (y == 0) || (y == (i - 1)))
+			{
+				cout << "||";
+			}
+			else
+			{
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
