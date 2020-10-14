4f2d3f68e9a56c3565d1f8d62d5ac6e047ffa838
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2020-10-14 12:10:35 +0300

    Add files via upload

---
+++ Крест.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "The cross of squares." << endl;
+	int k, l; cout << "Enter size: "; cin >> k;
+	l = k/2;
+	for (int x = 0;x < k;x++)
+	{
+		for (int y = 0;y < k;y++)
+		{
+			if ((y == 0) || (y == l) || (y == (k-1)) || (x == 0) || (x == l) || (x == (k-1)))
+			{
+				cout << "||";
+			}
+			else
+			{
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
