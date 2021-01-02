bde009ffbc36712854245be675a17e79de0e9590
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-01-02 22:17:16 +0300

    Add files via upload

---
+++ exam_cpp/Билет №10.2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class ItemNumbers 
+{
+public: static void print(int a[200]) 
+{
+	int k = 1; 
+	for (int i = 4; i < 200; i++)
+	{
+		if (a[i] == (a[i - 3] + a[i - 2] + a[i - 1]))
+		{
+			cout << k << ") " << a[i - 3] << " + " << a[i - 2] << " + " << a[i - 1] << " = " << a[i] << "\n";
+			k = k + 1;
+			if (k == 11) break;
+		}
+	}
+}
+};
+
+int main()
+{
+	cout << "Item numbers.\n\n"; 
+	int a[200];
+	for (int i = 0; i < 200; i++) 
+	{
+		a[i] = rand() % 11 - 5;
+		if ((i % 20 == 0) && (i != 0))
+		{
+			cout << "[" << a[i] << "] " << "\n";
+		}
+		else cout << "[" << a[i] << "] ";
+	}
+	cout << "\n\n";
+	ItemNumbers::print(a);
+}
